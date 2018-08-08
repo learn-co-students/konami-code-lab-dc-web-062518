969193fb8216c23b9b1ceda7618b1bf29c53aad6
finale100 <finale150@gmail.com>
2018-08-08 12:20:01 -0400

    done

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,17 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let input = document.querySelector('body')
+  let i = 0
+  input.addEventListener('keydown', function(e) {
+    if (e.key === codes[i]) {
+      i++
+      if (i === codes.length){
+        alert('Hey! You found the GOLDEN EGG')
+        i = 0
+      }
+    } else {
+      i = 0
+    }
+  })
 }
